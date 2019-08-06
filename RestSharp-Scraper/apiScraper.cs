d7105c8d8cf86ee39e9b54bea429a786a0f4fbc6
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-08-05 20:09:32 -0500

    restSharp Success. Was able to request and respond with stock fields and stock data and was inserted into SQL DB Table.

--- RestSharp-Scraper/apiScraper.cs
+++ RestSharp-Scraper/apiScraper.cs
@@ -23,16 +23,47 @@ namespace RestSharp_Scraper
             request.AddHeader("x-rapidapi-key", "bfab8a0181mshf12c4afb207144ap126f55jsnecf3c75cf681");
 
             IRestResponse response = client.Execute(request);
-            var content = response.Content;
+            var content = response.Content.ToString();
 
             // Console.WriteLine(content);
 
             dynamic stockDataAsJObject = JsonConvert.DeserializeObject(content);
 
-             Console.WriteLine(stockDataAsJObject["MarketRegions"]["USA"][0]["Exchange"]);
+            JArray USAStockData = stockDataAsJObject["MarketRegions"]["USA"];
 
-            //Console.WriteLine(stockDataAsJObject["MarketRegions"]);
+            // Console.WriteLine(USAStockData);
 
+            // string sym = Convert.ToString(stockDataAsJObject["MarketRegions"]["USA"][0]["Exchange"]);
+
+            // Console.WriteLine(stockDataAsJObject["MarketRegions"]["USA"][0]["Exchange"]);
+
+            string connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=apiDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            using (SqlConnection db = new SqlConnection(connectionString))
+            {
+                db.Open();
+                Console.WriteLine("Database has been opened!");
+                Console.WriteLine();
+
+                foreach (JToken stocks in USAStockData)
+                {
+                    SqlCommand insertion = new SqlCommand("INSERT INTO dbo.apiScrapeTable (StockRecord, Symbol, LastPrice, PercentChange, MarketChange) VALUES ( @stockRecord, @symbol, @lastPrice, @percentChange, @marketChange )", db);
+
+                    insertion.Parameters.AddWithValue("@stockRecord", DateTime.Now);
+                    insertion.Parameters.AddWithValue("@symbol", stocks["Exchange"].ToString());
+                    insertion.Parameters.AddWithValue("@lastPrice", stocks["Price"].ToString());
+                    insertion.Parameters.AddWithValue("@percentChange", stocks["PercentChange"].ToString());
+                    insertion.Parameters.AddWithValue("@marketChange", stocks["PriceChange"].ToString());
+
+                    insertion.ExecuteNonQuery();
+                }
+
+                db.Close();
+                Console.WriteLine("Database has been updated with data!");
+                Console.WriteLine();
+
+
+            }
 
         }
     }
